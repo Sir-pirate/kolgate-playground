6f833383d935105156df271a6529bd445e297f68
Pirate Ray <voc963@hotmail.com>
2020-08-17 01:30:13 -0400

    Updates

---
+++ precache-manifest.c395a493257fa8fd2e53ba901778f3cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e56be6954993e911ab5551f6f72d4210",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edc77737c9ea4bc6662c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9118fea2211c313c8c76",
+    "url": "/static/css/main.62afcbcc.chunk.css"
+  },
+  {
+    "revision": "edc77737c9ea4bc6662c",
+    "url": "/static/js/2.a55393b9.chunk.js"
+  },
+  {
+    "revision": "ac00860f10b301ce4ff76aaa802ce348",
+    "url": "/static/js/2.a55393b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9118fea2211c313c8c76",
+    "url": "/static/js/main.38378092.chunk.js"
+  },
+  {
+    "revision": "c2581e3de65f8e75105f",
+    "url": "/static/js/runtime-main.96aa887e.js"
+  },
+  {
+    "revision": "6ad4c00c1e4c0654a7f2ce780e04315b",
+    "url": "/static/media/durian.6ad4c00c.png"
+  }
+]);
